2c093f2851a2ecadd4c2ee7f8cdd92fea0e70d00
carlos <cgarciapieto@gmail.com>
2019-03-08 15:49:19 -0600

    afternoon

--- BootCRUDApp/urls.py
+++ BootCRUDApp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('additem/', views.additem, name='additem'),
+    path('index', views.index, name='index'),
+    path('', views.additem, name='additem'),
     path('additem/edit/<int:id>/', views.edititem, name='edit'),
 
 ]
